69981238e3a0168f9832203042486c3cc7d1cef6
MatheusPerondi <mathsap40@outlook.com>
2024-08-15 15:47:20 -0300

    .

--- src/main/java/com/client/ws/rasmooplus/model/UserType.java
+++ src/main/java/com/client/ws/rasmooplus/model/UserType.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 @Entity
 @Table(name = "user_type")
 public class UserType implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_type_id")
