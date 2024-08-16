e766c21c13f69d5690e3f3e6edae6782dc26cfec
MatheusPerondi <mathsap40@outlook.com>
2024-08-16 16:13:03 -0300

    ExceptionHandler, NotFoundException

--- src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -1,14 +1,12 @@
 package com.client.ws.rasmooplus.controller;
 
+import com.client.ws.rasmooplus.exception.NotFoundException;
 import com.client.ws.rasmooplus.model.SubscriptionType;
 import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -21,18 +19,18 @@ public class SubscriptionTypeController {
     private SubscriptionTypeService subscriptionTypeService;
 
     @GetMapping()
-    public ResponseEntity<List<SubscriptionType>> findAll(){
+    public ResponseEntity<List<SubscriptionType>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<SubscriptionType> findById(@PathVariable("id") Long id){
-        SubscriptionType subscriptionType = subscriptionTypeService.findById(id);
+    public ResponseEntity<SubscriptionType> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findById(id));
+    }
 
-        if (Objects.nonNull(subscriptionType)){
-            return ResponseEntity.status(HttpStatus.OK).body(subscriptionType);
-
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> notFoundException(NotFoundException n) {
+        String errorMessage = n.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body((errorMessage));
     }
 }
