3bdbfbab3be55704d2b7906f4725b91dd4c0c171
MatheusPerondi <mathsap40@outlook.com>
2024-08-26 15:21:07 -0300

    Validando entradas

--- src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
+++ src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
@@ -1,9 +1,10 @@
 package com.client.ws.rasmooplus.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -15,11 +16,17 @@ public class SubscriptionTypeDto {
 
     private Long id;
 
+    @NotBlank(message = "campo name não pode ser nulo ou vazio")
+    @Size(min = 5, max = 30)
     private String name;
 
+    @Max(value = 12, message = "campo acessMonth não pode ser maior que 12")
     private Long accessMonths;
 
+    @NotNull(message = "campo price não pode ser nulo")
     private BigDecimal price;
 
+    @NotBlank(message = "campo productKey não pode ser nulo ou vazio")
+    @Size(min = 5, max = 15, message = "campo productKey deve ter tamanho entre 5 e 15")
     private String productKey;
 }
